9869688d1dfd85495833f14969a897ef56eeddcb
Bombay <72895600+XiosBombay@users.noreply.github.com>
2021-02-26 02:48:16 -0500

    Update and rename SaltySyn_CarCrash/__resource.lua to fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,7 +1,11 @@
-resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+fx_version 'cerulean
+
+game 'gta5'
 
 description 'Blackout and damage vehicle on Vehicle Accident'
-version '1.1'
+
+version '1.2'
+
 client_scripts {
   'config.lua',
   'client.lua'
