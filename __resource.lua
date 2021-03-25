1dd834655b7385b7bfe78b622d594f1256b2f059
entarukun <76920369+entarukun@users.noreply.github.com>
2021-03-24 19:51:26 -0500

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,8 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+description 'Blackout and damage vehicle on Vehicle Accident'
+version '1.1'
+client_scripts {
+  'config.lua',
+  'client.lua'
+}
---
+++ config.lua
@@ -0,0 +1,25 @@
+Config = {}
+
+-- Amount of Time to Blackout, in milliseconds
+-- 2000 = 2 seconds
+Config.BlackoutTime = 2000
+
+
+-- Enable blacking out due to vehicle damage
+-- If a vehicle suffers an impact greater than the specified value, the player blacks out
+Config.BlackoutFromDamage = true 
+Config.BlackoutDamageRequired = 25
+
+-- Enable blacking out due to speed deceleration
+-- If a vehicle slows down rapidly over this threshold, the player blacks out
+Config.BlackoutFromSpeed = true  
+Config.BlackoutSpeedRequired = 60 -- Speed in MPH
+
+-- Enable the disabling of the vehicle incrementaly if a crash of player vehilce is detected
+Config.EnableVehicleCrashEffects = true
+
+Config.DamagePedOnCrash = false -- applies 20 damage to ped on car crash  
+Config.PedCrashDamage = 20 
+
+
+Config.randomTireBurstInterval = 100			-- Number of minutes (statistically, not precisely) to drive above 22 mph before you get a tire puncture. 0=feature is disabled
